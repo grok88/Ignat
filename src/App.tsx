55198b83705ca165e40a2ad124f2f5e390d0ec0d
Alex <grok88@tut.by>
2020-06-22 19:32:48 +0300

    Task2 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import DisplayTelegram from './components/display-telega/DisplayTelegtam';
+import {v1} from 'uuid';
+import PriorityList from './components/priority-list/PriorityList';
+
+//TS
+export type priorityListType = {
+    id: string,
+    title: string,
+    priority: string
+}
+export type stateType = Array<priorityListType>;
+export type filterType = 'all' | 'hight' | 'middle' | 'low';
 
 function App() {
+    let [state, setState] = useState<Array<priorityListType>>([
+        {id: v1(), title: 'react', priority: 'hight'},
+        {id: v1(), title: 'work', priority: 'hight'},
+        {id: v1(), title: 'anime', priority: 'low'},
+        {id: v1(), title: 'games', priority: 'middle'},
+        {id: v1(), title: 'js', priority: 'hight'},
+    ]);
+
+    let [filter, setFilter] = useState<filterType>('all');
+
+    //filtered
+    let filteredState = state;
+
+    if(filter === 'hight'){
+        filteredState = filteredState.filter(list => list.priority === 'hight');
+    }
+    if(filter === 'middle'){
+        filteredState = filteredState.filter(list => list.priority === 'middle');
+    }
+    if(filter === 'low'){
+        filteredState = filteredState.filter(list => list.priority === 'low');
+    }
+    //changr filter
+    const changeFilter = (value:filterType) => {
+        setFilter(value);
+    }
+
+    //delete priority list
+    const deleteItem = (id: string) => {
+        setState(state.filter(list => list.id !== id));
+    }
+
     return (
         <div className="App">
-            <DisplayTelegram fullName={'Александр Горячко'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aperiam architecto aspernatur at autem commodi consequuntur culpa cumque dolorem, dolores eius est eveniet exercitationem fuga, magni natus neque, nisi non nostrum omnis pariatur placeat praesentium quam quia quod rem reprehenderit rerum sequi similique sint vitae voluptate! At, deleniti!'} addedTime={'16:47'}/> <DisplayTelegram fullName={'Александр Горячко'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aperiam architecto aspernatur at autem commodi consequuntur culpa cumque dolorem, dolores eius est eveniet exercitationem fuga, magni natus neque, nisi non nostrum omnis pariatur placeat praesentium quam quia quod rem reprehenderit rerum sequi similique sint vitae voluptate! At, deleniti!'} addedTime={'16:47'}/> <DisplayTelegram fullName={'Александр Горячко'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias aliquam aperiam architecto aspernatur at autem commodi consequuntur culpa cumque dolorem, dolores eius est eveniet exercitationem fuga, magni natus neque, nisi non nostrum omnis pariatur placeat praesentium quam quia quod rem reprehenderit rerum sequi similique sint vitae voluptate! At, deleniti!'} addedTime={'16:47'}/>
+            <DisplayTelegram fullName={'Александр Горячко'}
+                             text={'Lorem ipsum dolor sit amet, consecteturuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu adipisicing elit. Accusantium alias aliquam aperiam architecto aspernatur at autem commodi consequuntur culpa cumque dolorem, dolores eius est eveniet exercitationem fuga, magni natus neque, nisi non nostrum omnis pariatur placeat praesentium quam quia quod rem reprehenderit rerum sequi similique sint vitae voluptate! At, deleniti!'}
+                             addedTime={'16:47'}/>
+            <PriorityList state={filteredState} deleteItem={deleteItem} changeFilter={changeFilter}/>
         </div>
     );
 }
---
+++ src/components/priority-list/PriorityList.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {priorityListType, filterType} from '../../App';
+
+type PriorityListPropsType = {
+    state: Array<priorityListType>,
+    deleteItem: (id: string) => void,
+    changeFilter: (value: filterType) => void
+}
+
+const PriorityList = (props: PriorityListPropsType) => {
+    let {state, deleteItem,changeFilter} = props;
+    const onChangeAllFilter = () => {
+        changeFilter('all');
+    }
+    const onChangeHightFilter = () => {
+        changeFilter('hight');
+    }
+    const onChangeMiddleFilter = () => {
+        changeFilter('middle');
+    }
+    const onChangeLowFilter = () => {
+        changeFilter('low');
+    }
+    return (
+        <div>
+            <h2>Lesson2</h2>
+            <ul>
+                {
+                    state.map(list => {
+                        return <li key={list.id}>
+                            {list.title}
+                            <button onClick={() => {
+                                deleteItem(list.id)
+                            }}>x
+                            </button>
+                        </li>
+                    })
+                }
+            </ul>
+            <div>
+                <button onClick={onChangeAllFilter}>All</button>
+                <button onClick={onChangeHightFilter}>Hight</button>
+                <button onClick={onChangeMiddleFilter}>Middle</button>
+                <button onClick={onChangeLowFilter}>Low</button>
+            </div>
+        </div>
+    );
+}
+export default PriorityList;
